f5322bd00b830a1c5eccc3522861dcf10de0825d
Viet Le <vietlq85@gmail.com>
2017-05-28 11:04:29 +0100

    slices

--- 01_basics/array02.go
+++ 01_basics/array02.go
@@ -17,6 +17,8 @@ func main() {
         "Claire",
     }
     fmt.Println(names)
+
+    // Slices do not store any data, they are merely views/references with boundaries
     a := names[0:2]
     b := names[1:3]
     b[0] = "Misha the Great"
