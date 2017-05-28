744e3cdcd35c39a51bb329de2637d8a9662f9787
Viet Le <vietlq85@gmail.com>
2017-05-28 14:40:05 +0100

    added reader

--- 01_basics/reader.go
+++ 01_basics/reader.go
@@ -6,6 +6,16 @@ import (
     "strings"
 )
 
+/*
+Readers
+The io package specifies the io.Reader interface, which represents the read end of a stream of data.
+The Go standard library contains many implementations of these interfaces, including files, network connections, compressors, ciphers, and others.
+The io.Reader interface has a Read method:
+func (T) Read(b []byte) (n int, err error)
+Read populates the given byte slice with data and returns the number of bytes populated and an error value. It returns an io.EOF error when the stream ends.
+The example code creates a strings.Reader and consumes its output 8 bytes at a time.
+*/
+
 func main() {
     r := strings.NewReader("Hello, Reader!")
 
