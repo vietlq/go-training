e5aae8193090008452ab7b59a67d5224c3f2a81d
Viet Le <vietlq85@gmail.com>
2017-05-28 10:45:19 +0100

    struct literals

--- 01_basics/struct01.go
+++ 01_basics/struct01.go
@@ -7,6 +7,14 @@ type Vertex struct {
     Y int
 }
 
+// Struct literals
+var (
+    v1 = Vertex{1, 2}  // has type Vertex
+    v2 = Vertex{X: 1}  // Y: 0 is implicit
+    v3 = Vertex{}      // implicit X:0 and Y:0
+    q  = &Vertex{3, 4} // has type *Vertex
+)
+
 func main() {
     v := Vertex{1, 2}
     fmt.Println(v)
