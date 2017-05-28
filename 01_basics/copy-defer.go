65f8bb974dee8e04468f734f0bf3e1eeff657b3e
Viet Le <vietlq85@gmail.com>
2017-05-28 10:06:20 +0100

    defer safe copy

---
+++ 01_basics/copy-defer.go
@@ -0,0 +1,27 @@
+package main
+
+// https://blog.golang.org/defer-panic-and-recover
+
+import (
+    "io"
+    "os"
+)
+
+func CopyFile(dstName, srcName string) (written int64, err error) {
+    src, err := os.Open(srcName)
+    if err != nil {
+        return
+    }
+    defer src.Close()
+
+    dst, err := os.Create(dstName)
+    if err != nil {
+        return
+    }
+    defer dst.Close()
+
+    return io.Copy(dst, src)
+}
+
+func main() {
+}
