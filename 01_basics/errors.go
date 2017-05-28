03d73a48cc4e5feb9517bc2c1965f5e46e689151
Viet Le <vietlq85@gmail.com>
2017-05-28 14:26:13 +0100

    Error()

---
+++ 01_basics/errors.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+import "time"
+
+/*
+Go programs express error state with error values.
+The error type is a built-in interface similar to fmt.Stringer:
+type error interface {
+    Error() string
+}
+*/
+
+type MyError struct {
+    When time.Time
+    What string
+}
+
+// Implement the method Error() string
+func (e *MyError) Error() string {
+    return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func run() error {
+    return &MyError{
+        time.Now(),
+        "it didn't work",
+    }
+}
+
+func main() {
+    if err := run(); err != nil {
+        fmt.Println(err)
+    }
+}
