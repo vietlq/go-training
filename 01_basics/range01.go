325f9b45872f4914e98071484cce77ff9a80e31e
Viet Le <vietlq85@gmail.com>
2017-05-28 11:25:30 +0100

    range

--- 01_basics/range01.go
+++ 01_basics/range01.go
@@ -8,4 +8,12 @@ func main() {
     for i, v := range pow {
         fmt.Printf("2**%d = %d\n", i, v)
     }
+
+    for i := range pow {
+        fmt.Println("i =", i)
+    }
+
+    for _, v := range pow {
+        fmt.Println("v = ", v)
+    }
 }
